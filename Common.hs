84577ae0d14128bcf52327f484b132be77ff8869
Gabina <gabinaluz@gmail.com>
2019-06-15 16:12:40 -0300

    Empezando el TP de ALP

---
+++ Common.hs
@@ -0,0 +1,8 @@
+module Common where
+
+import Control.Monad
+import System.IO (hFlush, stdout)
+
+type Verse = String
+
+type Poem = [Verse]
---
+++ Main.hs
@@ -0,0 +1,31 @@
+module Main where
+
+import Parsing
+import Common
+
+-- ::t print
+-- run cabal
+-- para ingresar un pormea:
+-- "tu culo es lo único redondo/ en este edificio de oficinas/*"
+readPoem :: Parser Poem
+readPoem = do char '*'
+              return ["***"]
+              <|>
+              do v <- readVerse
+                 p <- readPoem
+                 return (v:p)
+
+readVerse :: Parser Verse
+readVerse =  do c <- item
+                if c == '/'
+                   then return ""
+                   else do l <- readVerse
+                           return (c:l)
+                               
+main :: IO ()
+
+main = do   putStrLn "Bienvenido a Méri"    
+            putStrLn "Ingrese un poema. Para finalizar presione *"
+            x <- getLine
+            print x
+            --putStrLn "Andy!" 
